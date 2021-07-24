84d4c74522b7435420a9d86603ca6cc9b64f6c69
liupengy19 <liupy@88.com>
2021-07-24 20:11:10 +0800

    delete output

--- main.py
+++ main.py
@@ -200,54 +200,56 @@ def run_lr_W4_Nu20_M1_100_Na1():
 
 reslist = np.zeros(9)
 #! plot1
-def run_lr_W2_9_Nu3_M10_Na1():
+def run_lr_W2_10_Nu2_M4_Na5():
     for time in range(1000):
-        for n in range(2, 10):
+        for n in range(2, 10):#! change to 11
             reslist[n - 2] += (
                 random_m4_variance(
                     noisy_W(n, 0),
                     na=n // 2,
-                    NuA=3,
-                    NuB=3,
-                    Nm=10,
+                    NuA=2,
+                    NuB=2,
+                    Nm=4,
                     N_iid=1,
                     device=device,
                     scheme="l",
                     qU=qU,
+                    N_average=5
                 )
                 / 1000
             )
-    th.save(th.tensor(reslist), "./output/lr_W2_9_Nu3_M10_Na1")
+    np.save("./output/test",reslist)
 
 
-def run_gr_W2_9_Nu3_M10_Na1():
+def run_gr_W2_10_Nu2_M4_Na5():
     for time in range(1000):
-        for n in range(2, 10):
+        for n in range(2, 10):#! change to 11
             reslist[n - 2] += (
                 random_m4_variance(
                     noisy_W(n, 0),
                     na=n // 2,
-                    NuA=3,
-                    NuB=3,
-                    Nm=10,
+                    NuA=2,
+                    NuB=2,
+                    Nm=4,
                     N_iid=1,
                     device=device,
                     scheme="g",
                     qU=qU,
+                    N_average=5
                 )
                 / 1000
             )
-    th.save(th.tensor(reslist), "./output/gr_W2_9_Nu3_M10_Na1")
+    np.save("./output/test",reslist)
 
 
-def run_ls_W2_9_M60_Na1():
+def run_ls_W2_10_M80_Na1():
     for time in range(100):
-        for n in range(2, 10):
+        for n in range(2, 11):
             reslist[n - 2] += (
                 shadow_m4_variance(
                     noisy_W(n, 0),
                     na=n // 2,
-                    m=60,
+                    m=80,
                     N_iid=1,
                     device=device,
                     scheme="l",
@@ -255,25 +257,25 @@ def run_ls_W2_9_M60_Na1():
                 )
                 / 100
             )
-    th.save(th.tensor(reslist), "./output/ls_W2_9_M60_Na1")
+    np.save("./output/ls_W2_10_M80_Na1",reslist)
 
 
-def run_gs_W2_9_M60_Na1():
-    for time in range(10):
-        for n in range(2, 10):
+def run_gs_W2_10_M80_Na1():
+    for time in range(100):
+        for n in range(2, 11):
             reslist[n - 2] += (
                 shadow_m4_variance(
                     noisy_W(n, 0),
                     na=n // 2,
-                    m=60,
+                    m=80,
                     N_iid=1,
                     device=device,
                     scheme="g",
                     qU=qU,
                 )
-                / 10
+                / 100
             )
-    th.save(th.tensor(reslist), "./output/gs_W2_9_M60_Na1")
+    np.save("./output/gs_W2_10_M80_Na1",reslist)
 
 
 #! plot2
@@ -294,6 +296,6 @@ def run_ls_W4_M10_90_Na1():
 
 
 if __name__ == "__main__":
-    run_gs_W4_M10_90_Na1()
+    run_ls_W2_10_M80_Na1()
     terminate()
     draw(reslist, range(1, 10))
